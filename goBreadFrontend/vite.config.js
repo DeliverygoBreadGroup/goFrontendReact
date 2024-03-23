86e30f4d5236f468007ba14c31039a64f8850a54
Alexsandro Leite <110926439+Alequiso@users.noreply.github.com>
2024-03-22 22:09:11 -0300

    atualização pipipopo

--- goBreadFrontend/vite.config.js
+++ goBreadFrontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Ouvir em todas as interfaces de rede
+    port: 5173, // Opcional: Definir a porta, se desejar mudar da padrão (3000)
+  }
 })
